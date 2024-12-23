b8171670f1da6a8cb1763647e9121d08625cd221
Condruz Filip - Gabriel <condruz.filip@gmail.com>
2024-12-23 12:45:58 +0200

    Testing the quick commit & push mechanism

--- user-login.php
+++ user-login.php
@@ -1,10 +1,11 @@
 <?php
     include_once 'db/db.php';
     $db = DB::getInstance();
-    
+
     include 'modules/header.php';
 ?>
 
     <h1>User Login</h1>
 
 <?php include 'modules/footer.php'; ?>
+
